25ab937e68ee86219e118ed853fed60f357012f1
10sr <8slashes+git@gmail.com>
2013-12-15 23:56:38 +0900

    add tds.content.js

---
+++ static/tds-content.js
@@ -0,0 +1,22 @@
+TDSContent = (function (){
+    var ready = false;
+    function init(){
+        return;
+    }
+
+    function start(){
+        return;
+    }
+
+    function dequeue(){
+        return;
+    }
+
+    return {
+        init: init,
+        start: start,
+        dequeue: dequeue
+    };
+})();
+
+TDSContent.init();
